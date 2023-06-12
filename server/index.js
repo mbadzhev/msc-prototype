5d48de5386eec59ea0747a82da7fdf023a876dcf
Momchil Badzhev <mbadzhev@gmail.com>
2023-06-12 12:03:55 +0100

    Add environment variable as db uri

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/test");
+mongoose.connect(process.env.DATABASE_URI);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database."));
