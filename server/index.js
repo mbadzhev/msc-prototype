5c30b883ed4e0a7e25c9dd9955d66540d4ab789b
Momchil Badzhev <mbadzhev@gmail.com>
2023-06-19 11:23:49 +0100

    Update connectin to use net parser

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const app = express();
 const cors = require("cors");
 const port = 5000;
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URI);
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database."));
